53bf3c69b0aaf1c0f3dcf1e3c9c3ba2ccb62a43b
Ramin Dorosti <raminydo@gmail.com>
2025-08-17 12:43:40 +0330

    updated
    - Detector 3 is added. It's a new method for detection using merged answers of detector 1 and detector 2, and removed overlapping regions.
    - Detector 4 is added. This detector is using Selective Search method.

--- PREPROCESSING/detection_models.py
+++ PREPROCESSING/detection_models.py
@@ -8,7 +8,7 @@ import easyocr
 # custom method ------------------------
 def text_detection(image:object):
     """Text region detection based on contours, adaptive thresholding
-        and morphological view(a custom method without a model)
+        and morphological view(a classic method without a model)
 
     Args:
         image (object): numpy.ndarray
@@ -30,7 +30,7 @@ def text_detection(image:object):
 # easyocr method -------------------------
 reader = easyocr.Reader(['en', 'fa'])
 
-def text_detection_ocr(image):
+def text_detection_ocr(image:object):
     """Text region detection based on contours and EasyOCR
 
     Args:
@@ -39,9 +39,9 @@ def text_detection_ocr(image):
     Returns:
         list: contours
     """
-
-    img_rgb = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-    result = reader.readtext(img_rgb, decoder='greedy', paragraph=True)
+    
+    image_rgb = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    result = reader.readtext(image_rgb, decoder='greedy', paragraph=True)
 
     contours = []
     for detection in result:
@@ -50,3 +50,105 @@ def text_detection_ocr(image):
         contours.append(contour)
     
     return contours
+
+
+
+
+# merged method -------------------------
+def merged_detection(image:object):
+    """Text region detection; merged and optimized answers of text_detection()
+        and text_detection_ocr()
+
+    Args:
+        image (object): numpy.ndarray
+
+    Returns:
+        list: contours
+    """
+    # region inner function
+    # filter overlap borders of text_detection with text_detection_ocr
+    def filter_custom_by_ocr(custom_contours, ocr_contours):
+
+        def contour_inside(contour_small, contour_large, threshold=0.05):
+            inside_count = 0
+
+            for point in contour_small:
+                x, y = float(point[0][0]), float(point[0][1])
+
+                if cv.pointPolygonTest(contour_large, (x, y), False) >= 0:
+                    inside_count += 1
+                    
+            return inside_count / len(contour_small) >= threshold
+
+        filtered_custom = []
+        for c in custom_contours:
+            keep = True
+            for o in ocr_contours:
+                if contour_inside(c, o):
+                    keep = False
+                    break
+            if keep:
+                filtered_custom.append(c)
+
+        return filtered_custom
+        # endregion
+
+
+    con1 = text_detection(image)
+    con2 = text_detection_ocr(image)
+    filtered_con1 = filter_custom_by_ocr(con1, con2)
+
+    all_contours = list(filtered_con1) + list(con2)
+    boxes = [cv.boundingRect(c) for c in all_contours]
+
+    indices = cv.dnn.NMSBoxes(boxes, [1.0]*len(boxes), score_threshold=0.5, nms_threshold=0.3)
+    indices = np.array(indices).flatten()
+    merged_contours = [all_contours[i] for i in indices]
+
+    return merged_contours
+
+
+
+
+
+# Selective Search method -------------------------
+def text_detection_ss(image:object, min_size:int=500, aspect_ratio_range:tuple=(0.2, 5.0)):
+    """Text region detection based on Selective Search
+
+    Args:
+        image (object): numpy.ndarray
+        min_size (int, optional): minimum area(in pixels) of a region to be considered valid. Defaults to 500.
+        aspect_ratio_range (tuple, optional): acceptable range of width/height fore a region. Defaults to (0.2, 5.0).
+
+    Returns:
+        list: contours
+    """
+    ss = cv.ximgproc.segmentation.createSelectiveSearchSegmentation()
+    ss.setBaseImage(image)
+    ss.switchToSelectiveSearchQuality()
+
+    boxes = ss.process()
+    contours_ss = []
+
+    for (x, y, w, h) in boxes:
+        if w * h < min_size:
+            continue
+        aspect_ratio = w / float(h)
+        
+        if not (aspect_ratio_range[0] <= aspect_ratio <= aspect_ratio_range[1]):
+            continue
+        
+        contour = np.array([
+            [[x, y]],
+            [[x + w, y]],
+            [[x + w, y + h]],
+            [[x, y + h]]
+        ], dtype=np.int32)
+
+        contours_ss.append(contour)
+
+    return contours_ss[:10]
+
+
+
+
